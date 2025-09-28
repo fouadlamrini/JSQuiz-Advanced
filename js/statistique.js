c39eabc02e21a894644a85c1ce42eaa6006aad0b
fouad lmrini <fouad11.lamrini@gmail.com>
2025-09-28 23:58:41 +0100

    afficher affichage score moyen et Meilleur score global

--- js/statistique.js
+++ js/statistique.js
@@ -11,3 +11,24 @@ for (let theme in partiesParTheme) {
   parties.textContent += `${theme} : ${partiesParTheme[theme]}\n`;
 }
 
+//affichage score moyen
+let themesUniques = [...new Set(themes)]; 
+let scoreMoyenParTheme = {};
+themesUniques.forEach(theme => {
+  let joueursTheme = utilisateurs.filter(utilisateur => utilisateur.theme === theme);
+  
+  let total = joueursTheme.reduce((acc, j) => acc + j.score, 0);
+  let moyenne = total / joueursTheme.length;
+  scoreMoyenParTheme[theme] = moyenne;
+});
+
+let scoresDiv = document.getElementById("scores");
+
+for (let theme in scoreMoyenParTheme) {
+  scoresDiv.textContent += `${theme} : ${scoreMoyenParTheme[theme]}\n`;
+}
+//Meilleur score global 
+let scores = utilisateurs.map(utilisateur => utilisateur.score);
+let meilleurScore = scores.reduce((max, score) => Math.max(max, score), 0);
+let divMeilleur=document.getElementById('meilleur');
+divMeilleur.textContent=meilleurScore;
